1461396549c4902a4dd162ff0a2141a79a4d6d85
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-09 12:34:08 -0700

    Created Functions to Print Dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,24 @@
-filename = open("test.txt")# put your code here.
+def open_file(file):
 
-poem_dictionary = {}
-for line in filename:
-	line = line.rstrip()
-	line = line.split(" ")
+	filename = open(file)# put your code here.
 
-	for word in line:
-		poem_dictionary[word] = poem_dictionary.get(word,0) + 1
-#print(poem_dictionary)	
+	return filename
 
-for word, number in poem_dictionary.items():
-	print("{} {}".format(word, number))
+def create_dict(filename):
+	poem_dictionary = {}
+	
+	for line in filename:
+		line = line.rstrip()
+		line = line.split(" ")
+		for word in line:
+			poem_dictionary[word] = poem_dictionary.get(word,0) + 1
+	
+	return poem_dictionary
+
+def print_out_dict(poem_dictionary):
+	for word, number in poem_dictionary.items():
+		print("{} {}".format(word, number))
+
+#open_file("test.txt")
+print_out_dict(create_dict(open_file("test.txt")))
+print_out_dict(create_dict(open_file("twain.txt")))
