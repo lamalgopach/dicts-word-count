96195b667de373c256839fd774856d2813562ddb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-09 12:49:51 -0700

    Code review

--- wordcount.py
+++ wordcount.py
@@ -1,16 +1,17 @@
-def open_file(file):
+def open_file(filename):
 
-	filename = open(file)# put your code here.
+	file = open(filename)# put your code here.
 
-	return filename
+	return file
 
-def create_dict(filename):
+def create_dict(file):
 	poem_dictionary = {}
 	
-	for line in filename:
-		line = line.rstrip()
-		line = line.split(" ")
+	for line in file:
+		line = line.rstrip().split(" ")
+		#line = line.split(" ")
 		for word in line:
+			word = word.lower()
 			poem_dictionary[word] = poem_dictionary.get(word,0) + 1
 	
 	return poem_dictionary
@@ -20,5 +21,9 @@ def print_out_dict(poem_dictionary):
 		print("{} {}".format(word, number))
 
 #open_file("test.txt")
-print_out_dict(create_dict(open_file("test.txt")))
-print_out_dict(create_dict(open_file("twain.txt")))
+file1 = open_file("test.txt")
+word_dict = create_dict(file1)
+print_out_dict(word_dict)
+
+#print_out_dict(create_dict(open_file("test.txt")))
+#print_out_dict(create_dict(open_file("twain.txt")))
